7a711ace6f1f4ee1e12182d6c8431a16de30aafd
qwe123coder <72848513+qwe123coder@users.noreply.github.com>
2021-06-06 19:00:58 +0530

    Create console log .js

---
+++ console log .js
@@ -0,0 +1,12 @@
+console.log('hello world');
+let msg = “%c Hi 👋! Welcome to my bot!”; 
+let styles= [ 
+    ‘font-size: 12px’, 
+    ‘font-family: monospace’, 
+    ‘background: white’, 
+    ‘display: inline-block’, 
+    ‘color: black’, 
+    ‘padding: 8px 19px’, 
+    ‘border: 1px dashed;’ 
+].join(‘;’) 
+console.log(msg, styles);
